7105cc390e4bf68d08a3433081c4b295ac43a4a2
Camille Holt <camilleholt1@gmail.com>
2018-10-02 22:50:18 -0400

    Added an applet and html file
    Added an applet and html file

---
+++ JavaProject/src/NewApplet.java
@@ -0,0 +1,8 @@
+import java.applet.*;
+import java.awt.*;
+
+public class NewApplet extends Applet {
+   public void paint (Graphics gh) {
+      gh.drawString("Tutorialspoint.com", 300, 150);
+   }
+}
