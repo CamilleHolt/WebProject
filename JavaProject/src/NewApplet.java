f67e34d456a983a8c012148c63d9477867b95797
Camille Holt <camilleholt1@gmail.com>
2018-10-04 12:00:49 -0400

    Web1
    Web1

--- JavaProject/src/NewApplet.java
+++
@@ -1,8 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-
-public class NewApplet extends Applet {
-   public void paint (Graphics gh) {
-      gh.drawString("Tutorialspoint.com", 300, 150);
-   }
-}
---
+++ Web1/src/HelloServlet/HelloServlet.java
@@ -0,0 +1,44 @@
+package HelloServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.print("This is in the servlet");
+		//http://localhost:8080/Web1/HelloServlet
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
